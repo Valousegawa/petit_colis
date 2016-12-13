a4ef03f41e8f6b24f31c6af47d767c74d90402fb
Valentin <valou.pannacotta@gmail.com>
2016-12-13 16:28:44 +0100

    A lot of things
    index.html stylising
    testimonies
    a lot of css

--- app/app.js
+++ app/app.js
@@ -1,19 +1,55 @@
-var app = angular.module('petit_colis', ['pascalprecht.translate']);
-app.controller('HomeController', HomeController);
+/* MODULE DECLARATION */
+var app = angular.module('petit_colis', ['pascalprecht.translate', 'ngRoute']);
 
+/* CONTROLLER DECLARATION */
+app.controller('HomeController', HomeController);
+app.controller('mainController', function ($scope) {
+    $scope.temoignages = [
+        {nom : "RAMIREZ", prenom : "Sanchez", note : "5", message : "Super service ! Je recommande à tout ceux qui venlent envoyer pour pas cher et rapidement."},
+        {nom : "ANDREAS", prenom : "Louuis", note : "4", message : "Excellent rapport qualité prix !"},
+        {nom : "DURIFF", prenom : "Sylvain", note : "5", message : "Pour envoyer des vaisseaux de la Vierge Marie, y'a pas mieux !"},
+        {nom : "PACIFIC", prenom : "Sound3003", note : "5", message : "Han C'est de la bombe cet outil, je recommande au nom du silence de la MAsterCard."},
+    ]
+});
+app.controller('profileController', function ($scope) {
+});
+app.controller('addColisController', function ($scope) {
+});
+
+/* CONFIG DECLARATION */
 app.config(function ($translateProvider) {
     $translateProvider.useStaticFilesLoader({
         prefix: 'trad/locale-',
         suffix: '.json'
     });
-    $translateProvider.preferredLanguage('en_US');
+    $translateProvider.preferredLanguage('fr_FR');
+});
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/home.html',
+            controller: 'mainController'
+        })
+        .when('/profile', {
+            templateUrl: 'pages/profile.html',
+            controller: 'profileController'
+        })
+        .when('/ajouter_colis', {
+            templateUrl: 'pages/add_colis.html',
+            controller: 'addColisController'
+        });
+    $locationProvider.html5Mode(true);
 });
 
-function HomeController($translate, $scope){
-    $scope.language = 'en_US';
-    $scope.languages = ['en_US', 'fr_FR'];
+/* FACTORY DECLARATION */
 
-    $scope.updateLanguage = function() {
+
+/* FUNCTION DECLARATION */
+function HomeController($window, $translate, $scope) {
+    $scope.language = 'fr_FR';
+    $scope.languages = ['en_US', 'fr_FR', 'es_ES'];
+
+    $scope.updateLanguage = function () {
         $translate.use($scope.language);
     };
 }
