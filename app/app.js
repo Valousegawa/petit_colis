fcb3a43a75b972b31a03983e040bf0169b3fa848
Sébastien MINES <s.mines@alfun.fr>
2016-12-13 16:24:53 +0100

    Connection and Sign up
    Add files for the connection and sign up a account.
    Add the lastest version of JQuery.
    Add functions for connection and create account.

--- app/app.js
+++ app/app.js
@@ -1,5 +1,21 @@
 var app = angular.module('petit_colis', ['pascalprecht.translate']);
 app.controller('HomeController', HomeController);
+app.factory("ressources", ['$http', function($http){
+    var ressourceBase = "ressources/"
+    var obj = {};
+    obj.getUser = function(user){
+        return $http.post(ressourceBase + "login", user).then(function(results){
+            return results;
+        });
+    };
+    obj.inscription = function(users){
+        return $http.post(ressourceBase + "inscription", users).then(function(results){
+            return results;
+        });
+    };
+    return obj;
+}]);
+
 
 app.config(function ($translateProvider) {
     $translateProvider.useStaticFilesLoader({
@@ -9,11 +25,31 @@ app.config(function ($translateProvider) {
     $translateProvider.preferredLanguage('en_US');
 });
 
-function HomeController($translate, $scope){
+function HomeController($translate, $scope, ressources){
     $scope.language = 'en_US';
     $scope.languages = ['en_US', 'fr_FR'];
 
     $scope.updateLanguage = function() {
         $translate.use($scope.language);
+    };
+    $scope.connect = function(){
+        var data = [
+            $scope.genre,
+            $scope.user_prenom,
+            $scope.user_nom,
+            $scope.mail,
+            $scope.mdp,
+            $scope.naissance,
+            $scope.newsletter,
+            $scope.rang
+        ];
+        ressources.inscription(data);
+    };
+    $scope.submit = function(){
+        var donnees = [
+            $scope.email,
+            $scope.passwd,
+        ];
+        ressources.getUser(donnees);
     };
 }
